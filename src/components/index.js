c68877414d9f8e65e979eeaa6dd4cd203087766f
Chu Hoang Son <chuson1996@gmail.com>
2016-09-26 14:13:39 +0300

    Fix bugs

--- src/components/index.js
+++ src/components/index.js
@@ -5,17 +5,17 @@
  *
  */
 
-export CounterButton from './CounterButton/CounterButton';
-export GithubButton from './GithubButton/GithubButton';
-export InfoBar from './InfoBar/InfoBar';
-export MiniInfoBar from './MiniInfoBar/MiniInfoBar';
-export SurveyForm from './SurveyForm/SurveyForm';
-export WidgetForm from './WidgetForm/WidgetForm';
-export AddNewFolderForm from './AddNewFolderForm/AddNewFolderForm';
-export FolderCard from './FolderCard/FolderCard';
-export WordCard from './WordCard/WordCard';
-export EditButton from './EditButton/EditButton';
-export EditTermModal from './EditTermModal/EditTermModal';
-export AddNewTermForm from './AddNewTermForm/AddNewTermForm';
-export DefAndExCard from './DefAndExCard/DefAndExCard';
-export ImageSelect from './ImageSelect/ImageSelect';
+// export CounterButton from './CounterButton/CounterButton';
+// export GithubButton from './GithubButton/GithubButton';
+// export InfoBar from './InfoBar/InfoBar';
+// export MiniInfoBar from './MiniInfoBar/MiniInfoBar';
+// export SurveyForm from './SurveyForm/SurveyForm';
+// export WidgetForm from './WidgetForm/WidgetForm';
+// export AddNewFolderForm from './AddNewFolderForm/AddNewFolderForm';
+// export FolderCard from './FolderCard/FolderCard';
+// export WordCard from './WordCard/WordCard';
+// export EditButton from './EditButton/EditButton';
+// export EditTermModal from './EditTermModal/EditTermModal';
+// export AddNewTermForm from './AddNewTermForm/AddNewTermForm';
+// export DefAndExCard from './DefAndExCard/DefAndExCard';
+// export ImageSelect from './ImageSelect/ImageSelect';
