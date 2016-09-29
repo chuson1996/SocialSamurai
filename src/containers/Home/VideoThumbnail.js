f748f3e63893de00076f2632e4d196f1927cd2c5
DESKTOP-0DR0241\tvtri <tvtri96@yahoo.com.vn>
2016-09-29 23:51:01 +0300

    Update VideoThumbnail

--- src/containers/Home/VideoThumbnail.js
+++ src/containers/Home/VideoThumbnail.js
@@ -2,11 +2,11 @@ import React from 'react';
 // import Thumbnail from 'react-bootstrap/lib/Thumbnail';
 // import { Link } from 'react-router';
 
-const VideoThumbnail = ({src, description}) => {
+const VideoThumbnail = ({src, description, ...rest}) => {
     const styles = require('./Home.scss');
     // console.log(rest);
     return (
-        <a src={src} className="thumbnail">
+        <a className="thumbnail" {...rest}>
             <img src={src}/>
             <h3 className={styles['thumbnail-title']}>{description}</h3>
         </a>
