3b668e117d0cff3f7a10b8ffa5bc50b73591af56
Chu Hoang Son <chuson1996@gmail.com>
2016-10-11 13:38:58 +0300

    Nothing much

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 // import { Link } from 'react-router';
 // import config from '../../config';
 import Helmet from 'react-helmet';
@@ -7,8 +7,17 @@ import Grid from 'react-bootstrap/lib/Grid';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 import {LinkContainer} from 'react-router-bootstrap';
+import {connect} from 'react-redux';
+import get from 'lodash/get';
 
+@connect((state) => ({
+	challenges: get(state.session.data, 'challenges')
+}))
 export default class Home extends Component {
+	static propTypes = {
+		challenges: PropTypes.array
+	};
+
 	render() {
 		const styles = require('./Home.scss');
 
